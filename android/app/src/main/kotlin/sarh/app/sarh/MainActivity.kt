09e432140a5e3304420e65e748e00f07d5f13e0c
Skilledtech Imac <skilledtechimac@Skilledtechs-iMac.local>
2019-08-01 10:36:35 +0400

    init commit

---
+++ android/app/src/main/kotlin/sarh/app/sarh/MainActivity.kt
@@ -0,0 +1,13 @@
+package sarh.app.sarh
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
